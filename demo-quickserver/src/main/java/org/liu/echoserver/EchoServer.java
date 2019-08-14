0029f014284159e7f19a7d66c9e63e665e1f0cdd
刘章盛 <hello-ketty-honey@163.com>
2019-08-14 18:17:59 +0800

    增加些demo

--- demo-quickserver/src/main/java/org/liu/echoserver/EchoServer.java
+++ demo-quickserver/src/main/java/org/liu/echoserver/EchoServer.java
@@ -14,7 +14,7 @@ import java.util.logging.SimpleFormatter;
 
 public class EchoServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         String cmdHandler = "org.liu.echoserver.EchoCommandHandler";
         String authenticator = "org.liu.echoserver.EchoServerQuickAuthenticator";
         String authenticationHandler = "org.liu.echoserver.EchoClientAuthenticationHandler";
