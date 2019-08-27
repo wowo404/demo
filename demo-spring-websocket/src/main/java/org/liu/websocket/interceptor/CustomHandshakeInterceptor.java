77ed74a7be4030d567ebdf8e531bb4db1394085d
刘章盛 <hello-ketty-honey@163.com>
2019-08-27 15:09:40 +0800

    demo

--- demo-spring-websocket/src/main/java/org/liu/websocket/interceptor/CustomHandshakeInterceptor.java
+++ demo-spring-websocket/src/main/java/org/liu/websocket/interceptor/CustomHandshakeInterceptor.java
@@ -7,7 +7,6 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
