2cb8403730ad503a2c3a76b87f1a506940f33530
LiuZhangsheng <1879036165@qq.com>
2019-10-29 00:25:09 +0800

    1.demo

---
+++ demo-mina/src/main/java/org/demo/mina/TimeServerHandler.java
@@ -0,0 +1,31 @@
+package org.demo.mina;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+
+import java.util.Date;
+
+public class TimeServerHandler extends IoHandlerAdapter {
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+        System.out.println("IDLE" + session.getIdleCount(status));
+    }
+
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+        cause.printStackTrace();
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+        String msg = message.toString();
+        System.out.println("Receive message," + msg);
+        if ("quit".equalsIgnoreCase(msg)) {
+            session.closeNow();
+            return;
+        }
+        session.write(new Date().toString());
+        System.out.println("Message written...");
+    }
+}
